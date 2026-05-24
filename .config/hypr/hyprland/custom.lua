a283389c51efffd041c22f86e37282f786e76496
HarunaIzumi <haruharuna@outlook.com>
2026-05-24 18:55:32 -0300

    Various modifications to custom config

--- .config/hypr/hyprland/custom.lua
+++ .config/hypr/hyprland/custom.lua
@@ -2,24 +2,25 @@
 ---- CUSTOM ----
 ----------------
 
-local mainMod = "SUPER"
-
 -- Reload waybar with mainMod + ALT + R
-hl.bind(mainMod .. " + ALT + R", hl.dsp.exec_cmd("pkill waybar && hprctl dispatch exec waybar"))
+hl.bind("SUPER + ALT + R", hl.dsp.exec_cmd("killall -SIGUSR2 waybar"))
 
 -- Go to the previous workspace with ALT + TAB
--- hl.bind("ALT + TAB", hl.dsp.focus("last"))
+-- hl.bind("ALT + TAB", function()
+--   hl.dispatch(hl.dsp.window.cycle_next())
+--   hl.dispatch(hl.dsp.window.bring_to_top())
+-- end)
 
 -- Floating mini terminal with mainMod + T
-hl.bind(mainMod .. " + T", hl.dsp.exec_cmd("kitty", {float = true, size = {640, 480}, move = {"(monitor_w-window_w-5)", "(monitor_h-window_h-5)"}}))
+hl.bind("SUPER + T", hl.dsp.exec_cmd("kitty", {float = true, size = {640, 480}, move = {"(monitor_w-window_w-5)", "(monitor_h-window_h-5)"}}))
 
 -- Open Brower keybind
-hl.bind(mainMod .. " + B", hl.dsp.exec_cmd("helium-browser"))
+hl.bind("SUPER + B", hl.dsp.exec_cmd("helium-browser"))
 
 -- PrintScr Keybinds
-hl.bind(mainMod .. " + PRINT", hl.dsp.exec_cmd("hyprshot -m window -o ~/Pictures/Screenshots"))
+hl.bind("SUPER + PRINT", hl.dsp.exec_cmd("hyprshot -m window -o ~/Pictures/Screenshots"))
 hl.bind("PRINT", hl.dsp.exec_cmd("hyprshot -m output -o ~/Pictures/Screenshots"))
-hl.bind(mainMod .. " + SHIFT + PRINT", hl.dsp.exec_cmd("hyprshot -m region -o ~/Pictures/Screenshots"))
+hl.bind("SUPER + SHIFT + PRINT", hl.dsp.exec_cmd("hyprshot -m region -o ~/Pictures/Screenshots"))
 
 hl.window_rule({
   -- Window Rule for waybar modules
